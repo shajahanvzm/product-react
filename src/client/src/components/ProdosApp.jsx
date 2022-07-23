d6a25491a8ddea50d5bbe103aafef0a74e86d9e6
shajahanvzm <shajahanvzm@gmail.com>
2022-07-23 13:03:31 +0530

    adding first UI changes

---
+++ src/client/src/components/ProdosApp.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class ProdosApp extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Welcome, hello there PRODOS</h1>
+            </div>
+        );
+    }
+}
+
+export default ProdosApp;
