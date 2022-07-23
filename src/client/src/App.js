c6fb4fd0ca3d86d136f31aad053ee6f2f8afca7e
shajahanvzm <shajahanvzm@gmail.com>
2022-07-23 13:08:13 +0530

    addidng secound elemente

--- src/client/src/App.js
+++ src/client/src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
 import './components/ProdosApp'
 function App() {
   return (
     <div className="App">
       <h1>Welcome, hello there PRODOS</h1>
+      <h2>Product List</h2>
+      <p> this is product list</p>
     </div>
   );
 }
